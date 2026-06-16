fbf5302fb822bbb9c966f8795fa403e2d585dded
varshana <varshanasenthilnathan@gmail.com>
2026-06-16 12:21:02 +0530

    Initial calculator project

---
+++ calculator.py
@@ -0,0 +1,27 @@
+def calculator():
+    print("CALCULATOR")
+
+    try:
+        num1=float(input("enter num 1: "))
+        num2=float(input("enter num 2: "))
+        op=input("enter operator(+,-,%,/,*): ")
+        if (op == "+"):
+            sum=num1+num2
+            print(sum)
+        elif(op == "-"):
+            sub=num1-num2
+            print(sub)
+        elif(op == "%"):
+            mod=num1%num2
+            print(mod)
+        elif(op == "/"):
+            div=num1/num2
+            print(div)
+        elif(op =="*"):
+            mul=num1*num2
+            print(mul)
+        else:
+            print("invalid operator")
+    except ValueError:
+        print("Enter a valid number")
+calculator()
